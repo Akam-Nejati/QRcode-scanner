dbeda39fde24329887b69fb61a1e3ccbafe64a70
Akam <mr.akam.320@gmail.com>
2023-07-30 23:06:26 +0330

    add createQr directory

--- src/components/createQr.vue
+++ src/components/createQr.vue
@@ -1,7 +1,20 @@
 <script setup  lang="ts">
 import { ref } from 'vue';
+import QrcodeVue from 'qrcode.vue'
 
 const modalShow = ref<boolean>(false)
+const qrValue = ref<string>()
+
+function downloadQrCode() {
+    const svg: any = document.querySelector(".svg")
+    const serializer = new XMLSerializer();
+    const svgString = serializer.serializeToString(svg);
+    const dataUri = 'data:image/svg+xml;base64,' + btoa(svgString);
+    const link = document.createElement('a');
+    link.href = dataUri;
+    link.download = `${qrValue.value}.svg`;
+    link.click();
+}
 </script>
 
 <template>
@@ -12,13 +25,19 @@ const modalShow = ref<boolean>(false)
 
     <Transition>
         <div @click.self="modalShow = false" v-if="modalShow"
-            class="w-full h-screen bg-[#00000071] absolute top-0 left-0 flex justify-center items-center px-4">
-            <div class="grid grid-cols-6 sm:grid-cols-4 gap-4">
-                <input type="text" class="col-span-4 sm:col-span-3 p-4 rounded-xl bg-slate-300 outline-none font-bold text-xl text-slate-900">
-                <button type="button" @click="modalShow = true"
-                    class="w-full col-span-2 sm:col-span-1 h-full bg-slate-300 text-slate-900 text-lg sm:text-xl font-bold rounded-xl shadow-xl flex justify-center items-center cursor-pointer">
-                    generate
-                </button>
+            class="w-full h-screen bg-[#000000b4] absolute top-0 left-0 flex flex-col justify-center items-center px-4">
+            <div>
+                <div class="grid grid-cols-6 sm:grid-cols-4 gap-4">
+                    <input v-model="qrValue" type="text"
+                        class="col-span-4 sm:col-span-3 p-4 rounded-xl bg-slate-300 outline-none font-bold text-xl text-slate-900">
+                    <button type="button" @click="downloadQrCode" ref="download" class="w-full col-span-2 sm:col-span-1 h-full bg-slate-300 text-slate-900 text-lg sm:text-xl font-bold rounded-xl shadow-xl flex justify-center items-center cursor-pointer">
+                        Download
+                    </button>
+                </div>
+                <div v-show="qrValue" class="w-full flex justify-center mt-8">
+                    <qrcode-vue :value="qrValue" :size="300" level="H" class="svg shadow-xl !rounded-xl" background="black"
+                        foreground="#CBD5E1" render-as="svg" />
+                </div>
             </div>
         </div>
     </Transition>
